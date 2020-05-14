24f346d83da1600ed53a37cca53981cbf78b1be4
Timothy Chea <tac.chea@gmail.com>
2020-05-13 21:55:54 -0400

    hellomate

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@ class Program
     static void Main(string[] args)
     {
         // Displays "Hello World!" on the terminal.
-        System.Console.WriteLine("Hello Tim!");
+        System.Console.WriteLine("Hello mate!");
     }
 }
